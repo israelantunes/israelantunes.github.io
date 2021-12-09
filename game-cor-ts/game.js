5971e212e155968b10cd143f6c095e7205ccbd14
Israel Antunes <49755889+israelantunes@users.noreply.github.com>
2021-12-09 20:42:56 -0300

    game-cor-ts

---
+++ game-cor-ts/game.js
@@ -0,0 +1,38 @@
+document.querySelector('#iniciar').addEventListener('click', adivinhacao);
+var arrCores = [
+    'Red','Blue','Green','Yellow','Magenta','Cyan','Grey','Black','Brown','White','Violet','Indigo'
+];
+function adivinhacao() {
+    var coresAleatorias;
+    var vetorCores = [];
+    for (var i = 0; i < 10; i++) {
+        coresAleatorias = Math.floor((Math.random() * arrCores.length) + 1);
+        vetorCores.push(arrCores[coresAleatorias]);
+    }
+    vetorCores.sort();
+    coresAleatorias = Math.floor((Math.random() * (vetorCores.length)) + 1);
+    var sorteada = vetorCores[coresAleatorias];
+    var chances = 5;
+    var inputUser;
+
+    while (chances > 0) {
+        inputUser = prompt("Eu escolhi apenas uma dessas cores: ".concat(vetorCores.join(", "), " \n\nVoc\u00EA tem ").concat(chances, " chances de adivinhar a cor! \nDigite uma:"));
+        if (inputUser == sorteada) {
+            alert("Você acertou, parabéns!");
+            document.body.style.background = sorteada;
+            chances = 0;
+        }
+        else if (inputUser == "") {
+            alert("O campo está em branco.\nPreencha o campo e tente novamente!");
+        }
+        else {
+            if (inputUser > sorteada) {
+                alert("Você errou!\n\nSua cor é alfabeticamente maior que a minha.");
+            }
+            else if (inputUser < sorteada) {
+                alert("Você errou!\n\nSua cor é alfabeticamente menor que a minha.");
+            }
+            chances--;
+        }
+    }
+}
---
+++ game-cor-ts/game.ts
@@ -0,0 +1,45 @@
+document.querySelector('#iniciar').addEventListener('click', adivinhacao);
+
+let arrCores: Array <string> = [
+    'Red','Blue','Green','Yellow','Magenta','Cyan','Grey','Black','Brown','White','Violet','Indigo'
+];
+
+
+function adivinhacao(): void {
+	let coresAleatorias: number;
+	let vetorCores: Array <string> = [];
+
+	for (let i = 0; i < 10; i++) {
+		coresAleatorias = Math.floor((Math.random() * arrCores.length) + 1);
+		vetorCores.push(arrCores[coresAleatorias]);
+	}
+
+	vetorCores.sort();
+
+	coresAleatorias = Math.floor((Math.random() * (vetorCores.length)) + 1);
+	let Sorteada: string = vetorCores[coresAleatorias]
+
+	let chances: number = 5;
+	let inputUser: string;
+	// console.log(corSorteada);
+
+	while (chances > 0) {
+		inputUser = prompt(`Eu escolhi apenas uma dessas cores: ${vetorCores.join(", ")} \n\nVocê tem ${chances} chances de adivinhar a cor! \nDigite uma:`);
+
+		if (inputUser == Sorteada) {
+			alert("Você acertou, parabéns!")
+			document.body.style.background = Sorteada;
+			chances = 0;
+		} else if (inputUser == "") {
+			alert("O campo está em branco.\nPreencha o campo e tente novamente!");
+		} else {
+			if (inputUser > Sorteada) {
+				alert("Você errou!\n\nSua cor é alfabeticamente maior que a minha.");
+			} else if (inputUser < Sorteada) {
+				alert("Você errou!\n\nSua cor é alfabeticamente menor que a minha.");
+			}
+			chances--;
+		}
+	}
+	
+}
